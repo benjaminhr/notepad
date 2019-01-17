105c3ca61bd64e8f3a34271c6f18ed795bedfc19
benjaminr177 <unoceptik@gmail.com>
2019-01-17 23:40:25 +0000

    use native menu/toolbar, read file contents

--- src/Application.py
+++ src/Application.py
@@ -1,7 +1,7 @@
 # here the main applciation will live
 # mainly for imported a class, etc and using it once
 
-from tkinter import Tk, Frame, Button, Text, INSERT, LEFT, X, BOTH
+from tkinter import Tk, Frame, Button, INSERT, LEFT, X, BOTH, Menu
 from .File import File
 
 def start():
@@ -14,22 +14,29 @@ def start():
   # forces window to front
   window.attributes("-topmost", True)
 
-  # padx and pady set x and y padding
-  toolbar = Frame(window)
-  toolbar.pack(fill=X, padx=7, pady=7)
+  menu = Menu(window)
+  window.config(menu=menu)
 
-  openFileBtn = Button(toolbar, text="Open File", command=File.open)
-  openFileBtn.pack(side=LEFT)
+  file = Menu(menu)
+  file.add_command(label = 'Open', command = File.open)
+  file.add_command(label = 'Save', command = File.save)
+  file.add_command(label = 'Exit', command = lambda:exit())
+  menu.add_cascade(label = 'File', menu = file)
+
+  window.mainloop()
+
+  # padx and pady set x and y padding
+  # toolbar = Frame(window)
+  # toolbar.pack(fill=X, padx=7, pady=7)
+
+  # create textbox with file contents and name
+
+  # openFileBtn = Button(toolbar, text="Open File", command=File.open)
+  # openFileBtn.pack(side=LEFT)
 
   # padding is added to space out buttons
-  saveFileBtn = Button(toolbar, text="Save File", command=File.save)
-  saveFileBtn.pack(side=LEFT, padx=5)
+  # saveFileBtn = Button(toolbar, text="Save File", command=File.save)
+  # saveFileBtn.pack(side=LEFT, padx=5)
 
-  textBox = Text(window)
-  # pady is (0,7) as there are there are 7px of spcaing on the toolbar
-  textBox.pack(expand=True, fill=BOTH, padx=7, pady=(0,7))
-
-  textBox.insert(INSERT,"Hello there...\nHow are you Benjamin?")
-
+  
   # keeps window open
-  window.mainloop()
--- src/File.py
+++ src/File.py
@@ -1,6 +1,13 @@
-from tkinter import filedialog
+from tkinter import filedialog, Text
+from tkinter.filedialog import askopenfilename
 
 class File:
+  def updateTextBox(contents):
+    textBox = Text(window)
+    # pady is (0,7) as there are there are 7px of spcaing on the toolbar
+    textBox.pack(expand=True, fill=BOTH, padx=7, pady=(0,7))
+    textBox.insert(INSERT, contents | "Type here...")
+
   def save():
     # NOTICE: It can throw an exception if you close the panel
     fileToSaveAs = filedialog.asksaveasfilename(
@@ -10,7 +17,18 @@ class File:
     
     print("Saving text to file " + fileToSaveAs)
   
-  def open():
-    # NOTICE: It can throw an exception if you close the panel
-    file = filedialog.askopenfilename(title = "Select file")
-    print("Opening file " + file)
+  def open(window):
+    name = askopenfilename(
+      filetypes = (("Text File", "*.txt"), ("All Files","*.*")),
+      title = "Choose a file."
+    )
+    print ("file name: " + name)
+    
+    # try if unknown file or closes without choosing a file
+    try:
+      with open(name, 'r') as uploadedFile:
+        contents = uploadedFile.read()
+        File.updateTextBox(contents)
+        print(contents)
+    except Exception as e: 
+      print(e)
