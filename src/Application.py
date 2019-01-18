e4c3ee3d1fcf93a22ecb27609a58c6a0c4ed70b9
Karmjit Mahil <45123383+JKaffe@users.noreply.github.com>
2019-01-18 20:29:13 +0000

    Improve file saving feature + Refactorw7

--- src/Application.py
+++ src/Application.py
@@ -3,34 +3,63 @@ from tkinter import messagebox
 from .File import File
 import atexit
 
+# Will contain the states for the application.
 class App:
-  def __init__(self, window):    
+
+  def __init__(self):
+    self.filePath = None
+
+  @property
+  def filePath(self):
+    return self.__filePath
+
+  @filePath.setter
+  def filePath(self, filePath):
+    self.__filePath = filePath
+
+class GUI(App):
+
+  def __init__(self, window):
+    App.__init__(self)
+
+    window.title("Notepad")
+    window.geometry("550x700")
+    window.attributes("-topmost", True)
     self.window = window
-    self.textbox = None
-  
-  def start(self):
-    self.window.title("Notepad")
-    self.window.geometry("550x700")
-    self.window.attributes("-topmost", True)
 
-    self.createMenu()
-    self.createTextbox()
-    atexit.register(self.saveBeforeExit)    
+    self.menu = Menu(self.window)
+    self.window.config(menu=self.menu)
+    self.__setupMenu(self.menu)
 
-  def createMenu(self):
-    menu = Menu(self.window)
-    self.window.config(menu=menu)
+    self.textbox = Text(window)
+    GUI.__setupTextbox(self.textbox)
 
+    atexit.register(self.saveBeforeExit)
+
+  def setWindowTitle(self, title):
+    self.window.title(title)
+
+  @property
+  def text(self):
+    # "end-1c" removes extra new line character that a normal end adds 
+    return self.textbox.get("1.0", "end-1c")
+
+  @text.setter
+  def text(self, text):
+    self.textbox.delete('1.0', END)
+    self.textbox.insert(INSERT, text or "Type here...")
+
+  def __setupMenu(self, menu):
     file = Menu(menu)
-    file.add_command(label = 'Open', command = lambda: File.open(self.window, self.textbox))
-    file.add_command(label = 'Save', command = lambda: File.save(self.window, self.textbox))
+    file.add_command(label = 'Open', command = lambda: File.open(self))
+    file.add_command(label = 'Save', command = lambda: File.save(self))
+    file.add_command(label = 'Save As', command = lambda: File.saveAs(self))
     file.add_command(label = 'Exit', command = lambda: exit())
     menu.add_cascade(label = 'File', menu = file)
 
-  def createTextbox(self):
-    self.textbox = Text(self.window)
-    self.textbox.pack(expand=True, fill=BOTH, pady=(0,7))
-    self.textbox.config(
+  def __setupTextbox(textbox):
+    textbox.pack(expand=True, fill=BOTH, pady=(0,7))
+    textbox.config(
       padx = 25,
       pady = 25, 
       borderwidth = 0,
@@ -46,19 +75,22 @@ class App:
     )
   
   def saveBeforeExit(self):
-    userWantsToSave = messagebox.askyesno(
-      "Unsaved Text", 
-      "Do you want to save the text before exiting?"
-    )
+    userWantsToSave = False
+
+    if self.text:
+      userWantsToSave = messagebox.askyesno(
+        "Unsaved Text", 
+        "Do you want to save the text before exiting?"
+      )
     
     if userWantsToSave:
-      File.save(self.window, self.textbox)
+      File.save(self)
     else:
       print("Exited without saving")
 
 def start():
   window = Tk()
-  App(window).start()
+  GUI(window)
   window.mainloop()
   
  
--- src/File.py
+++ src/File.py
@@ -3,39 +3,53 @@ from tkinter import *
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 
 class File:
-  def updateTextBox(textbox, contents):
-    textbox.delete('1.0', END)
-    textbox.insert(INSERT, contents or "Type here...")
+  def save(app, path=None):
+    if path is None and app.filePath is None:
+      path = asksaveasfilename(title = "Select file")
+      path = ''.join(path)
+      app.filePath = path
+    elif path is None:
+      path = app.filePath
 
-  def save(window, textbox):
-    path = asksaveasfilename(title = "Select file")
-    path = ''.join(path)
-    # "end-1c" removes extra new line character that a normal end adds
-    contents = textbox.get("1.0", "end-1c")
+    print("PT:" + path)
+    print(app.filePath)
+
+    contents = app.text
 
     try:
       with open(path, 'w') as toSaveAsFile:
         print("Saving text to path " + path)
         head, strippedPath = os.path.split(path)
-        window.title(strippedPath)
+        app.setWindowTitle(strippedPath)
         toSaveAsFile.write(contents);
 
     except FileNotFoundError as e:
         print("Could not save text to file")
         print(e)
 
-  def open(window, textbox):
+  def saveAs(app):
+      path = asksaveasfilename(title = "Select file")
+      path = ''.join(path)
+
+      if not path is None:
+        app.filePath = path
+        File.save(app, path)
+      
+
+  def open(app):
     path = askopenfilename(title = "Choose a file.")
-    path = ''.join(path)   
+    path = ''.join(path)
+
+    app.filePath = path 
 
     try:
       with open(path, 'r') as uploadedFile:
         contents = uploadedFile.read()
-        File.updateTextBox(textbox, contents)
-        head, strippedPath = os.path.split(path)
-        window.title(strippedPath)
-        
+        app.text = contents
         print ("Reading file  " + path)
+
+        head, strippedPath = os.path.split(path)
+        app.setWindowTitle(strippedPath)
 
     except Exception as e:
       messagebox.showerror("Error", "Could not open file.")
